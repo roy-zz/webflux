c54a28e286de95d142571827281eb8f409658ea0
roy-zz <74298434+roy-zz@users.noreply.github.com>
2022-04-22 18:57:13 +0900

    Java & Spring 비동기 기술 중간 커밋

---
+++ src/test/java/com/roy/webflux/reactivestream/asyncspring/AsyncJavaSpringTest1.java
@@ -0,0 +1,267 @@
+package com.roy.webflux.reactivestream.asyncspring;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.concurrent.*;
+
+@Slf4j
+class AsyncJavaSpringTest1 {
+
+    // 아래의 코드와 같이 실행하게 되면 HELLO와 EXIT는 어떻게 출력될까
+    // 애플리케이션이 실행되고 2초 후에 한번에 HELLO와 EXIT가 출력될 것이다.
+    static class OnlyMainThreadTest {
+        public static void main(String[] args) throws InterruptedException {
+            TimeUnit.SECONDS.sleep(2);
+            log.info("HELLO");
+            log.info("EXIT");
+        }
+    }
+
+    // 우리는 "EXIT"는 바로 출력되고 "HELLO"만 2초 후에 출력되기를 바란다.
+    // 아래와 같이 스레드를 "sleep"하는 코드와 `HELLO`를 출력하는 코드를 ExecutorService를 통해서 다른 스레드에서 실행시킨다.
+    // 출력된 결과를 확인해보면 애플리케이션 실행시점에 `EXIT`가 출력되었고 2초 후에 `HELLO`가 출력되었다.
+    // 스레드의 `sleep`을 사용할 때 InterruptedException을 따로 처리해야하는
+    // 이유는 스레드가 `sleep`인 상태에서 Owner 스레드가 실행 중인 스레드를 종료 및 인터럽트 하는 경우 `InterruptedException`이 발생하기 때문이다.
+    static class UseOtherThreadTest {
+        public static void main(String[] args) throws InterruptedException {
+            ExecutorService es = Executors.newCachedThreadPool();
+            es.execute(() -> {
+                try {
+                    TimeUnit.SECONDS.sleep(2);
+                    log.info("HELLO");
+                } catch (InterruptedException e) {}
+            });
+            log.info("EXIT");
+        }
+    }
+
+    // 이번에는 다른 스레드에서 수행되는 로직이 2초 후에 `HELLO`라는 문자열을 출력하도록 구현해본다.
+    // Callable은 리턴 값이 있지만 Runnable은 리턴 값이 없다.
+    // Return 값이 있다는 것은 메인 스레드에서 다른 스레드의 Return값을 사용하겠다는 의미가 되며 예외가 발생하는 경우 예외를 다른 스레드로 throw 할 수도 있다.
+    // Return 값을 바로 사용할 수 있는 것은 아니고 우리가 계속 언급하였던 Future 객체를 사용하여 받을 수 있다.
+    // 이러한 경우 `execute`메서드가 아닌 `submit`메서드를 사용한다.
+
+    // 출력되는 순서를 보면 이전에 다른 스레드에서 HELLO를 출력한 것과는 결과가 다르다는 점이다.
+    // 결국 메인 스레드는 Future의 get 메서드를 호출하면서 다른 스레드의 결과를 가져올 때까지 **블로킹**되어 있었다는 의미가 된다.
+    // 만약 Future의 get 메서드가 메인 스레드를 중지시키지 않고 null과 같은 값을 일단 반환한다면 **넌블러킹** 방식이 된다.
+    static class UseOtherThreadWithReturnTest {
+        public static void main(String[] args) throws ExecutionException, InterruptedException {
+            ExecutorService es = Executors.newCachedThreadPool();
+            Future<String> future = es.submit(() -> {
+                TimeUnit.SECONDS.sleep(2);
+                log.info("Async");
+                return "HELLO";
+            });
+            log.info("Future: {}", future.get());
+            log.info("EXIT");
+        }
+    }
+
+    // 만약 우리의 메인 스레드가 HELLO를 출력하는 스레드와 동시에 작동하는 상황이라고 가정하고 `EXIT`를 출력하는 코드와 Futere를 get하는 코드의 위치를 바꿔본다.
+    // 결과는 애플리케이션 실행과 동시에 `EXIT`가 실행되고 2초 후에 `HELLO`가 출력되었다.
+    // 다른 스레드에서 2초 동안 진행되는 작업과 메인 스레드에서의 작업이 동시에 진행되었음을 알 수 있다.
+    @Test
+    void manyThreadParallelTest() throws ExecutionException, InterruptedException {
+        log.info("Start Time: {}", LocalDateTime.now());
+        ExecutorService es = Executors.newCachedThreadPool();
+        Future<String> future1 = es.submit(() -> {
+            TimeUnit.SECONDS.sleep(2);
+            log.info("Async-1");
+            return "HELLO-1";
+        });
+        Future<String> future2 = es.submit(() -> {
+            TimeUnit.SECONDS.sleep(2);
+            log.info("Async-2");
+            return "HELLO-2";
+        });
+        Future<String> future3 = es.submit(() -> {
+            TimeUnit.SECONDS.sleep(2);
+            log.info("Async-3");
+            return "HELLO-3";
+        });
+        log.info("EXIT");
+        log.info("Future-1: {}", future1.get());
+        log.info("Future-2: {}", future2.get());
+        log.info("Future-3: {}", future3.get());
+        log.info("End Time: {}", LocalDateTime.now());
+    }
+
+    // Future의 `isDone()` 메서드는 작업 진행여부를 확인한다.
+    // `get()` 메서드와는 다르게 블로킹 시키지 않기 때문에 작업이 끝났는지 확인을 해보고 작업이 끝나지 않은 경우 다른 작업을 진행하고 돌아와서 다시 작업 진행유무를 확인할 수 있다.
+    // 예시의 출력을 보면 첫번째 isDone은 작업이 끝나지 않았으므로 false를 반환하고 두번째 isDone은 작업을 끝났음을 의미하는 false를 반환한다.
+    // 결국 Future는 결과를 얻어올 수 있는 핸들러와 같은 역할을 하는 것이지 결과 자체는 아니다.
+    @Test
+    void isDoneTest() throws InterruptedException, ExecutionException {
+        ExecutorService es = Executors.newCachedThreadPool();
+        Future<String> future = es.submit(() -> {
+            TimeUnit.SECONDS.sleep(2);
+            log.info("Async");
+            return "HELLO";
+        });
+        log.info("Future.isDone - 1: {}", future.isDone());
+        TimeUnit.SECONDS.sleep(3);
+        log.info("EXIT");
+        log.info("Future.isDone - 2: {}", future.isDone());
+        log.info("Future.get: {}", future.get());
+    }
+
+    // Future와 비슷한 방식으로 Javascript에서 많이 사용하는 Callback이 있으며 비동기 프로그래밍을 구현할 때 많이 사용되는 방식이다.
+    // 자바에서는 가장 기본적으로 Future를 사용하거나 Callback을 사용한다.
+    // 자바 1.8 이전에는 Future가 대표적으로 사용되었으나 결과를 가져올 때(get을 호출할 때) 블로킹된다는 사실에 많은 개발자들이 당황했다.
+    // 차라리 객체 자체를 전달하고 작업이 완료되면 메서드를 호출해달라고 요청하는 것이 훨씬 익숙한 패턴(커맨드패턴)이다.
+    // 지금부터 Future 자체를 Object로 만들어서 Callback과 유사하게 만들어본다.
+    // FutureTask는 Future를 구현체며 위에서 살펴본 것과 동일한 결과를 출력한다.
+    @Test
+    void futureTaskTest() throws InterruptedException, ExecutionException {
+        ExecutorService es = Executors.newCachedThreadPool();
+        FutureTask<String> futureTask = new FutureTask<String>(() -> {
+            TimeUnit.SECONDS.sleep(2);
+            log.info("Async");
+            return "HELLO";
+        });
+        es.execute(futureTask);
+        log.info("Future.isDone - 1: {}", futureTask.isDone());
+        TimeUnit.SECONDS.sleep(3);
+        log.info("EXIT");
+        log.info("Future.isDone - 2: {}", futureTask.isDone());
+        log.info("Future.get: {}", futureTask.get());
+    }
+
+    // ExecutorService.shutdown은 진행 중인 작업이 끝나면 스레드를 종료(Graceful하게)하라는 의미이며 비동기 작업 자체가 종료되지는 않는다.
+    // 이번에는 FutureTask를 익명 클래스로 만들어본다.
+    // FutureTask의 done 메서드는 작업이 모두 완료되면 호출되는 hook같은 존재다.
+    // 우리는 결과를 따로 출력하지 않았지만 done에 의해 HELLO가 출력되는 것을 볼 수 있다.
+    // FutureTask 또한 Future와 동일하게 자바 1.5부터 추가되어 있었으며 Callback과 같이 사용할 수 있는 기법들이 이미 구현되어 있다.
+    // 물론 매번 오버라이딩하여 구현해야한다는 점에서 자연스러운 Callback이라고 보기는 어렵다.
+    static class AnonymousFutureTaskTest {
+        public static void main(String[] args) {
+            ExecutorService es = Executors.newCachedThreadPool();
+            FutureTask<String> futureTask = new FutureTask<String>(() -> {
+                TimeUnit.SECONDS.sleep(2);
+                log.info("Async");
+                return "HELLO";
+            }) {
+                @Override
+                protected void done() {
+                    try {
+                        log.info("Call done: {}", get());
+                    } catch (InterruptedException | ExecutionException e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+            es.execute(futureTask);
+            es.shutdown();
+        }
+    }
+
+    // 이번에는 FutureTask를 사용하여 자연스러운 Callback이 되도록 CallbackFutureTask 메서드를 작성해본다.
+    // Objects의 requireNonNull은 검증하는 대상이 null인 경우 즉시 NullPointerException이 발생하기 때문에 null을 방어하기 위해 많이 사용된다.
+    // Callback 인터페이스인 SuccessCallback을 생성한다.
+    // 이렇게 생성하는 방식은 자바 1.8 이전까지는 불가능했다.
+    interface SuccessCallback {
+        void onSuccess(String result);
+    }
+
+    static class CallbackFutureTask extends FutureTask<String> {
+        private final SuccessCallback successCallback;
+        public CallbackFutureTask(Callable<String> callable, SuccessCallback successCallback) {
+            super(callable);
+            this.successCallback = Objects.requireNonNull(successCallback);
+        }
+        @Override
+        protected void done() {
+            try {
+                successCallback.onSuccess(get());
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    static class CallbackFutureTaskTest {
+        public static void main(String[] args) {
+            ExecutorService es = Executors.newCachedThreadPool();
+            CallbackFutureTask callbackFutureTask = new CallbackFutureTask(() -> {
+                TimeUnit.SECONDS.sleep(2);
+                log.info("Async");
+                return "HELLO";
+            }, result -> {
+                log.info("SuccessCallback.onSuccess: {}", result);
+            });
+            es.execute(callbackFutureTask);
+            es.shutdown();
+        }
+    }
+
+    // 우리는 이전 예제에서 CallbackFutureTask를 생성하여 Callback 형식으로 비동기로 작업을 처리하는 방법에 대해서 알아보았다.
+    // 하지만 원하는 결과만 얻어올 뿐 예외가 발생했을 때에 대한 예외처리는 따로 작성하지 않았다. 다른 스레드에서 발생한 예외를 우리는 메인 스레드로 끌어와야한다.
+    // 최근에는 try catch를 사용하는 것보다 Exception Object를 Exception을 처리하는 Callback에 던져주는 방식으로 많이 사용된다.
+    // 이번에는 예외를 처리하는 ExceptionCallback을 만들고 CallbackFutureTask가 ExceptionCallback을 생성자 파라미터로 받을 수 있도록 변경해본다.
+    interface ExceptionCallback {
+        void onError(Throwable t);
+    }
+
+    // FutureTask의 get을 호출할 때 InterruptedException과 ExecutionException을 처리해야한다.
+    // InterruptedException의 경우 다른 Exception들과는 성향이 다르다.
+    // 단순히 Interrupt 메시지를 받아도 발생하는 예외이기 때문에 스레드에 다시 인터럽트를 걸어서 처리해도 무관하다.
+    // ExecutionException은 비동기 작업을 수행하다가 예외 상황이 생겼을 때 발생하며 우리가 처리해줘야한다.
+    // 발생한 예외를 ExceptionCallback에게 전달할 때 바로 ExecutionException은 한 번 가공된 예외이기 때문에 getCause를 사용하여 정확한 원인을 전달해야한다.
+    static class CallbackFutureHandleExceptionTask extends FutureTask<String> {
+        private final SuccessCallback successCallback;
+        private final ExceptionCallback exceptionCallback;
+        public CallbackFutureHandleExceptionTask(Callable<String> callable,
+                                                 SuccessCallback successCallback,
+                                                 ExceptionCallback exceptionCallback) {
+            super(callable);
+            this.successCallback = Objects.requireNonNull(successCallback);
+            this.exceptionCallback = Objects.requireNonNull(exceptionCallback);
+        }
+        @Override
+        protected void done() {
+            try {
+                successCallback.onSuccess(get());
+            } catch (InterruptedException ie) {
+                Thread.currentThread().interrupt();
+            } catch (ExecutionException ee) {
+                exceptionCallback.onError(ee.getCause());
+            }
+        }
+    }
+
+    static class CallbackFutureHandleExceptionTaskTest {
+        public static void main(String[] args) {
+            ExecutorService es = Executors.newCachedThreadPool();
+            CallbackFutureHandleExceptionTask task = new CallbackFutureHandleExceptionTask(() -> {
+                TimeUnit.SECONDS.sleep(2);
+                if (true) throw new RuntimeException("Async Error Occurred");
+                log.info("Async");
+                return "HELLO";
+            },
+                    result -> log.info("onSuccess: {}", result),
+                    error -> log.error("onError: {}", error.getMessage()));
+            es.execute(task);
+            es.shutdown();
+        }
+    }
+
+    // 지금까지 우리는 Future에 대해서 알아보았다.
+    // 정리해보면 비동기 작업의 결과를 가져오는 방법은 두가지가 있다.
+    // 첫번째로 Future와 같이 작업의 결과를 담고 있는 핸들러를 리턴받아서 처리하는 방식이 있다. get메서드를 호출할 때 블로킹이 발생하며 예외가 발생하는 경우는 try catch를 사용하여 해결해야한다.
+    // 두번째로 Callback을 구현하여 사용하는 방법이 있다.
+
+    // 자바에서는 어떻게 비동기를 구현하고 있는지 AsyncchronousByChannel코드를 확인해 본다.
+    // read 메서드를 확인해보면 CompletionHandler를 전달하게 해준다.
+    // CompletionHandler 내부에는 completed와 failed 두 개의 메서드가 있으며 지금까지 우리가 구현한 방식과 동일하다.
+
+    // AsyncchronousByChannel 메서드 중 결과값이 Future<Integer>인 메서드도 있다.
+    // 위에서 살펴본 것과 같이 Future는 바로 반환이 되지만 get을 호출하는 순간 스레드는 블로킹된다.
+
+    // 지금까지 우리가 만들었던 로직을 살펴보면 비동기를 위한 로직과 서비스를 위한 로직이 동일한 위치에 존재하며 스프링이 지향하는 AOP와는 거리가 먼 코드가 작성되는 문제가 있다.
+    // 다음 장에서는 이러한 문제를 스프링의 옛 기술(15년 전)을 사용하여 해결해본다.
+    // 스프링은 이러한 문제를 @Async라는 애노테이션을 사용하여 해결한다.
+
+}
---
+++ src/test/java/com/roy/webflux/reactivestream/asyncspring/AsyncJavaSpringTest3.java
@@ -0,0 +1,105 @@
+package com.roy.webflux.reactivestream.asyncspring;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@EnableAsync
+@SpringBootApplication
+public class AsyncJavaSpringTest3 {
+    // Servlet 3.0: 비동기 서블릿
+    // HTTP Connection은 논블록킹 IO
+    // 서블릿 요청 읽기, 응답 쓰기는 블록킹
+    // 비동기 작업 시작 즉시 서블릿 스레드 반납
+    // 비동기 작업이 완료되면 서블릿 스레드 재할당
+    // 비동기 서블릿 컨텍스트 이용 (AsyncContext)
+    // Servlet 3.1: 논블록킹 IO
+    // 논블록킹 서블릿 요청, 응답 처리
+    // Callback
+    //
+    // 서블릿 3.0이 넘어가면서 이미 서블릿 위에서 동작하는 스프링 프레임워크를 사용하게 되었다.
+    // 서블릿 3.0에는 비동기적으로 서블릿 요청을 처리하는 기능이 추가되었다.
+    // 기본적으로 서블릿은 블록킹 구조로 이루어져있다. 블록킹이 I/O에서 가지는 큰 의미는 I/O가 발생할 때마다 서블릿에서 스레드를 하나씩 할당 해주었다.
+    // 여기서 I/O는 HTTP로 브라우저가 요청을 하는 것, 요청을 받아서 결과를 반환하는 것을 의미한다.
+    // 동시에 커넥션이 100개가 생기면 스레드 또한 100개가 사용되었으며 블로킹 방식에서는 어쩔 수 없는 구조다.
+    // 서블릿이 기본적으로 블로킹 구조인 이유는 HTTP 커넥션과 연관되어 있는 HttpServletRequest와 HttpServletResponse 두 개가 있는데
+    // 내부적으로 InputStream, OutputStream을 구현했고 여기서 사용되는 read와 같은 메서드는 기본적으로 블로킹 I/O 방식이다.
+    // read를 사용한 순간에 I/O쪽에 아무런 데이터가 없으면 대기하고 있다가 어떠한 데이터가 반환되거나 커넥션이 끊어지는 경우에 반환된다.
+    // 스레드가 블로킹되는 순간 CPU는 스레드를 대기상태로 전환(스위칭 한 번)하고 다시 작업을 시작할 때 Running 상태로 바꾸면서 전환(스위칭 한 번)이 발생하면서 총 두 번의 Context Switching이 발생하게 된다.
+    // 이렇게 스레드 블로킹을 많이 사용하게 되면 스레드간 Context Switching이 많이 일어나게 되고 불필요하게 많은 CPU 자원을 사용하게 된다.
+
+    // 우리가 사용하는 스프링의 기본적으로 사용되는 HttpServletRequest와 HttpServletResponse는 기본적으로 InputStream을 사용하고 있다.
+    // 이미 NIO를 사용하여 이러한 한계를 극복해보려 하였지만 큰 성과는 없었다.
+    // 사실 스레드 하나에서 request -> 서비스 로직 -> response를 처리하는 것은 큰 문제가 되지 않는다.
+    // 문제가 되는 상황은 request -> DB Connection or 외부 API 통신 -> response 과 같이 중간에 오래걸리는 작업이 중간에 들어가게 되면
+    // 해당 요청을 처리하는 스레드는 요청이 처리되기 전까지 불필요한 Context Switching을 유발하며 블로킹 상태가 된다.
+    // 이렇게 블로킹 상태가 된 스레드가 많아지면 결국 스레드는 전부 고갈될 것이고 클라이언트의 요청은 Queue에 쌓이게 될 것이다.
+    // Queue까지 가득차게 되면 클라이언트는 서비스 불가라는 응답을 받게 된다. Latency는 높아지고 에러율이 높아지는 상태가 된다.
+
+    // 스레드를 많이 만들면 되는거 아니냐는 의문이 발생할 수 있지만 자바에서 스레드는 내부적으로 Stack Trace를 가지고 있으며 이것은 메모리를 사용하는 것을 의미한다.
+    // 만약 많아진 요청을 스레드의 수를 늘려서 해결한다면 우리는 OutOfMemory 에러를 만나게 될 것이다.
+    // 또한 스레드가 많아지면 Contenxt Switching이 기존보다 훨씬 많이 발생하게 되면서 CPU 사용량이 치솟게 되며 오히려 응답속도가 느려질 수 있다.
+    // 결국 스레드를 많이 만드는 것은 근본적인 해결 방법이 아니다.
+
+    // 우리는 이러한 문제를 해결하기 위한 방법으로 Servlet 스레드가 블로킹I/O를 만났을 때 응답을 대기하고 있는 것이 아니라 바로 스레드 풀로 반환되어 다음 요청을 처리하게 해야하며 이러한 구조로 만드는 것이 비동기 서블릿이라는 기술이다.
+    // 스프링 3.0에서 나온 비동기 서블릿 기술을 스프링 3.1 기술에서 많은 부분 보완하였고 논블로킹 웹개발을 위한 기반이 만들어졌다.
+
+    // 스프링에서 어떠한 방식으로 논블로킹 웹개발을 가능하게 하였는지 살펴본다.
+    // 가장 앞단의 NIO Connector가 클라이언트의 요청을 받는다.
+    // NIO Connector는 서블릿 스레드를 만들거나 서블릿 스레드 풀에서 스레드를 가져온다.
+    // 만약 스레드의 작업이 오래걸리거나 @Async와 같은 애노테이션을 사용하여 작업 스레드에게 작업을 위임하면 바로 스레드 풀로 반환된다.
+    // 문제는 처리 중이던 "서블릿 스레드를 반납을 하면 누가 HTTP응답을 처리할 것이냐"인데
+    // 이 부분은 비동기 서블릿 엔진 자체적으로 작업 스레드가 작업을 마치고 반환하는 시점에 스레드 풀에서 스레드를 재할당하여 NIO Connector에게 응답을 반환하고 바로 서블릿 스레드를 반납한다.
+    // 이러한 메커니즘을 활용하여 적은 스레드의 수로 동시에 많은 요청을 처리할 수 있다.
+
+    // 지금부터 어떠한 방식으로 비동기 서블릿 엔진이 작동하는지 코드로 알아보도록 한다.
+
+    @Bean
+    protected ThreadPoolTaskExecutor springExecutor() {
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        taskExecutor.setCorePoolSize(10);
+        taskExecutor.setMaxPoolSize(20);
+        taskExecutor.setQueueCapacity(50);
+        taskExecutor.setThreadNamePrefix("Spring-Thread-");
+        return taskExecutor;
+    }
+
+    @RestController
+    @RequiredArgsConstructor
+    public static class MyController {
+        private final MySpringService mySpringService;
+
+        @GetMapping("/async")
+        public String async() {
+            return null;
+        }
+    }
+
+    @Service
+    public static class MySpringService {
+        @Async
+        public ListenableFuture<String> hello() throws InterruptedException {
+            log.info("Call hello()");
+            TimeUnit.SECONDS.sleep(1);
+            return new AsyncResult<>("HELLO");
+        }
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsyncJavaSpringTest3.class, args);
+    }
+
+}
